08cec2854bdf1ea3f2a6a924b0bde5f8a69de97e
MACILLAS <max.midwinter@gmail.com>
2023-03-29 16:37:20 -0400

    changed image location

--- scripts/pull_mk.sh
+++ scripts/pull_mk.sh
@@ -4,6 +4,6 @@ echo Ask ADMIN to obtain KEY...
 
 cat GAR_R_KEY.json | docker login -u _json_key --password-stdin https://us-east1-docker.pkg.dev
 
-docker pull us-east1-docker.pkg.dev/webinspector-370718/docker/mapping_kit/mapping_kit-ig3:latest
+docker pull us-east1-docker.pkg.dev/webinspector-370718/docker/mapping_kit-ig3:latest
 
-docker tag us-east1-docker.pkg.dev/webinspector-370718/docker/mapping_kit/mapping_kit-ig3:latest mapping_kit-ig3:latest
+docker tag us-east1-docker.pkg.dev/webinspector-370718/docker/mapping_kit-ig3:latest mapping_kit-ig3:latest
